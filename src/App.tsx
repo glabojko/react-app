ea1caf237a40f835bfb26625f25cb97bba325bd4
glabojko <glabojko@gmail.com>
2023-11-07 20:57:35 +0100

    stage and alert

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
 import Message from "./Message";
+import Alert from "./components/Alert";
 import ListGroup from "./components/ListGroup"; 
 
 function App() {
-  let items = ["Ruda", "Zabrze", "Bytom", "Świony", "Kato"];
+  // let items = ["Ruda", "Zabrze", "Bytom", "Świony", "Kato"];
+
+  // const handleSelectItem = (item: string) => {
+  //   console.log(item);
+  // }
   return (
   <div>
-    <ListGroup items={items} heading="Miasta" />
+    {/* <ListGroup items={items} heading="Miasta" onSelectItem={handleSelectItem}/> */}
+    <Alert />
     </div>
   );
   // <div><Message></Message></div> 
